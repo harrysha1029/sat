225c90e2caa68d200102345c7f2f94155ff394c7
Milan Mosse <mmosse19@users.noreply.github.com>
2020-07-06 00:40:29 -0700

    Update short_dnf.py

--- tests/short_dnf.py
+++ tests/short_dnf.py
@@ -1,8 +1,29 @@
+import itertools
+
+from src.random_sat import sample
+from src.sat_algs import all_solutions
+
 # num variables, clause width, num clauses
 n = 10, k = 3, m = 8
 
-# obtain k-CNF
+num_free_bits = 3
+num_samples  = 10
+true_blanks = [1,2,3]
 
-# encode
+# obtain k-CNFs
+phis = sample(num_samples, dist_R, n, k, m)
 
-# decode
+for phi in phis:
+  phi_blanks = impose_blanks(phi, true_blanks)
+  
+  # TODO: find maximally sensitive partial encoding x
+  candidates = all_solutions(phi_blanks)
+
+  # TODO: encode x with parallel_ppz
+
+  # attempt to decode with all different blanks
+  for blanks in list(itertools.combinations(range(1,n+1), 3)):
+    psi = impose_blanks(phi, list(blanks))
+    
+    # TODO: attempt to decode psi
+  
